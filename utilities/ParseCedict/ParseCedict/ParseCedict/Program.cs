9f3db698428fbf43ab9f8e38f63f17d03a67aaec
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2020-10-07 18:55:40 -0400

    Broke Regex and Fixed it again
    All that's left is to pretty-print it into a JSON file supporting hanzi

--- utilities/ParseCedict/ParseCedict/ParseCedict/Program.cs
+++ utilities/ParseCedict/ParseCedict/ParseCedict/Program.cs
@@ -16,12 +16,12 @@ namespace ParseCedict
     class Word
     {
         // All words must contain a hanzi, pinyin, english, part of speech, but may not have image path
-        public List<string> HanziSimp { get; set; } // Simplified Chinese character
-        public List<string> HanziTrad = new List<string>(); // Traditional Chinese character
-        public List<string> Pinyin { get; set; }    // Pinyin for the Word
-        public List<string> English { get; set; }   // English translation of the Word
-        public List<string> POS { get; set; }       // Part of speech of the Word
-        public List<string> ImagePath { get; set; }   // Path leading to the appropriate image for the Hanzi
+        public string HanziSimp; // Simplified Chinese character
+        public string HanziTrad; // Traditional Chinese character
+        public string Pinyin;    // Pinyin for the Word
+        public string English;   // English translation of the Word
+        public string POS;       // Part of speech of the Word
+        public string ImagePath; // Path leading to the appropriate image for the Hanzi
 
 
         /// <summary>
@@ -50,34 +50,33 @@ namespace ParseCedict
 
             Word word = new Word();
 
-            List<string> hanziTrad = new List<string>();
-            List<string> hanziSimp = new List<string>();
-            List<string> pinyin = new List<string>();
-            List<string> english = new List<string>();
+            string hanziTrad;
+            string hanziSimp;
+            string pinyin;
+            string english;
 
-            //Regex r = new Regex(@"(?<trad>[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]/+) (?<simp>[[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]/]+) (?<pinyin>\[([\w ]+)\]) (?<english>\/([\w ]+)\/)");
-            //Regex r = new Regex(@"^(?<trad>[\u4E00-\u9FCC]|[\u3400-\u4DB5]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6]|\ud869|[\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34]|\ud86d|[\udf40-\udfff]|\ud86e[\udc00-\udc1d]+) (?<simp>[\u4E00-\u9FCC]+|[\u3400-\u4DB5]+|[\ud840-\ud868]+[\udc00-\udfff]+|\ud869+[\udc00-\uded6]+|\ud869[\udf00-\udfff]+|[\ud86a-\ud86c]+[\udc00-\udfff]+|\ud86d+|[\udc00-\udf34]+|\ud86d+[\udf40-\udfff]+|\ud86e+|[\udc00-\udc1d]+) (?<pinyin>\[([\w ]+)\]) (?<english>\/([\w ]+)\/)$");
-            Regex r = new Regex(@"^(?<trad>[\w]+) (?<simp>[\w]+) (?<pinyin>\[([\w ]+)\]) (?<english>\/([\w ]+)\/)$");
+            Regex r = new Regex(@"^(?<trad>[\u4E00-\u9FA5]+) (?<simp>[\u4E00-\u9FA5]+) (?<pinyin>\[([\w ]+)\]) (?<english>\/(.+)\/)$");
 
             foreach (Match match in Regex.Matches(s, r.ToString(), RegexOptions.IgnoreCase))
             {
                 Console.WriteLine("Found: {0} : {1}", match.Groups["simp"].Value, match.Groups["english"]);
 
-                hanziTrad.Add(match.Groups["trad"].Value);
-                hanziSimp.Add(match.Groups["simp"].Value);
-                pinyin.Add(match.Groups["pinyin"].Value);
-                english.Add(match.Groups["english"].Value);
+                hanziTrad = match.Groups["trad"].Value;
+                hanziSimp = match.Groups["simp"].Value;
+                pinyin = match.Groups["pinyin"].Value;
+                english = match.Groups["english"].Value;
 
-                word.HanziTrad.Add(hanziTrad.Last());
+                word.HanziTrad = hanziTrad;
+                word.HanziSimp = hanziSimp;
+                word.Pinyin = pinyin;
+                word.English = english;
             }
 
             return word;
-
         }
 
         private static void SplitCeDict(string dictPath)
         {
-            // I want : [[一月份 一月份 [yi1 yue4 fen4] /January/], [一望無垠 一望无垠 [yi1 wang4 wu2 yin2] /to stretch as far as the eye can see (idiom)/], ...]
             List<string> lineList = new List<string>();
 
             const Int32 BufferSize = 128;
@@ -91,18 +90,6 @@ namespace ParseCedict
                     words.Add(CreateWord(l));
                 }
             }
-                    
-
-            
-
-            //string hanzi = lines[0]
-            
-            /*
-            File.WriteAllText(@"C:\_repo\han-zidian\utilities\ParseCedict\Parsed.json", PrettyJson(JsonConvert.SerializeObject(new Word("Jeff", 22))));
-            Console.WriteLine(@"Created JSON file at C:\_repo\han-zidian\utilities\ParseCedict");*/
-
-
-
         }
 
         private static string PrettyJson(string inJson)
@@ -120,10 +107,16 @@ namespace ParseCedict
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            //SplitCeDict(@"C:\_repo\han-zidian\utilities\ParseCedict\TestObj.txt");
-            Console.WriteLine(CreateWord("三三 三三三 [da2] /a vista of a dragon in flight/").HanziSimp.First<string>());
+            SplitCeDict(@"C:\_repo\han-zidian\utilities\ParseCedict\TestDict.txt");
+            foreach (var l in words)
+            {
+                File.WriteAllText(@"C:\_repo\han-zidian\utilities\ParseCedict\Parsed.json", PrettyJson(JsonConvert.SerializeObject(l)));
+            }
+            
+            Console.WriteLine(@"Created JSON file at C:\_repo\han-zidian\utilities\ParseCedict");
         }
     }
 }
-// TODO: Split cedict line by line, add each line to a list, add each list to one list
-// TODO: Split each line into the appropriate groups (hanzi, pinyin, etc.)
+
+// TODO: Make JSON file support Hanzi
+// TODO: Exception handling when parsing 118,000 lines of text. Something will probably happen.
